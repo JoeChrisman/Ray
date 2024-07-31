04708e2bddcc33ffbeb5ddc67849462efc7e98a4
Joe Chrisman <jchrisman@mail.csuchico.edu>
2024-07-31 03:47:14 -0700

    resolver square calculations

--- MoveGen.c
+++ MoveGen.c
@@ -1,6 +1,129 @@
 #include "MoveGen.h"
 #include "AttackTables.h"
 
+U64 getWhitePawnAttacks(U64 pawns)
+{
+    const U64 eastAttacks = BOARD_NORTH_EAST(pawns) & NOT_A_FILE;
+    const U64 westAttacks = BOARD_NORTH_WEST(pawns) & NOT_H_FILE;
+    return eastAttacks | westAttacks;
+}
+
+U64 getBlackPawnAttacks(U64 pawns)
+{
+    const U64 eastAttacks = BOARD_SOUTH_EAST(pawns) & NOT_A_FILE;
+    const U64 westAttacks = BOARD_SOUTH_WEST(pawns) & NOT_H_FILE;
+    return eastAttacks | westAttacks;
+}
+
+U64 getResolverSquares(
+    int checkedKing,
+    U64 checkingPawns,
+    U64 attackerKnights,
+    U64 attackerBishops,
+    U64 attackerRooks,
+    U64 attackerQueens)
+{
+    const U64 cardinalRays = getCardinalSlidingMoves(checkedKing, EMPTY_BOARD);
+    const U64 ordinalRays = getOrdinalSlidingMoves(checkedKing, EMPTY_BOARD);
+    const U64 cardinalAttackers = (attackerRooks | attackerQueens) & cardinalRays;
+    const U64 ordinalAttackers = (attackerBishops | attackerQueens) & ordinalRays;
+    U64 attackers = cardinalAttackers | ordinalAttackers;
+    attackers |= knightAttacks[checkedKing] & attackerKnights;
+    attackers |= checkingPawns;
+
+    U64 resolvers = EMPTY_BOARD;
+    if (attackers)
+    {
+        if (GET_NUM_PIECES(attackers) == 1)
+        {
+            if (cardinalAttackers)
+            {
+                resolvers = getCardinalSlidingMoves(GET_SQUARE(attackers), EMPTY_BOARD);
+                resolvers &= cardinalRays;
+                resolvers |= attackers;
+            }
+            else if (ordinalAttackers)
+            {
+                resolvers = getOrdinalSlidingMoves(GET_SQUARE(attackers), EMPTY_BOARD);
+                resolvers &= ordinalRays;
+                resolvers |= attackers;
+            }
+            else
+            {
+                resolvers = attackers;
+            }
+        }
+        else
+        {
+            resolvers = EMPTY_BOARD;
+        }
+    }
+    else
+    {
+        resolvers = FULL_BOARD;
+    }
+    return resolvers;
+}
+
+void updateLegalityInfo()
+{
+    int friendlyKingSquare;
+    U64 friendlyKing;
+    U64 friendlies;
+    U64 enemyPawnAttacks;
+    U64 enemyPawnAttackers;
+    U64 enemyKnights;
+    U64 enemyBishops;
+    U64 enemyRooks;
+    U64 enemyQueens;
+    U64 enemyKing;
+
+    if (position.isWhitesTurn)
+    {
+        friendlyKing = position.boards[WHITE_KING];
+        friendlyKingSquare = GET_SQUARE(friendlyKing);
+        friendlies = position.white;
+        enemyPawnAttacks = getBlackPawnAttacks(position.boards[BLACK_PAWN]);
+        enemyPawnAttackers = getWhitePawnAttacks(friendlyKing) & position.boards[BLACK_PAWN],
+        enemyKnights = position.boards[BLACK_KNIGHT];
+        enemyBishops = position.boards[BLACK_BISHOP];
+        enemyRooks = position.boards[BLACK_ROOK];
+        enemyQueens = position.boards[BLACK_QUEEN];
+        enemyKing = position.boards[BLACK_KING];
+    }
+    else
+    {
+        friendlyKing = position.boards[BLACK_KING];
+        friendlyKingSquare = GET_SQUARE(friendlyKing);
+        friendlies = position.black;
+        enemyPawnAttacks = getWhitePawnAttacks(position.boards[WHITE_PAWN]);
+        enemyPawnAttackers = getBlackPawnAttacks(friendlyKing) & position.boards[WHITE_PAWN],
+        enemyKnights = position.boards[WHITE_KNIGHT];
+        enemyBishops = position.boards[WHITE_BISHOP];
+        enemyRooks = position.boards[WHITE_ROOK];
+        enemyQueens = position.boards[WHITE_QUEEN];
+        enemyKing = position.boards[WHITE_KING];
+    }
+
+    safe = ~getAttacks(
+        friendlyKing,
+        friendlies,
+        enemyPawnAttacks,
+        enemyKnights,
+        enemyBishops,
+        enemyRooks,
+        enemyQueens,
+        enemyKing);
+
+    resolvers = getResolverSquares(
+        friendlyKingSquare,
+        enemyPawnAttackers,
+        enemyKnights,
+        enemyBishops,
+        enemyRooks,
+        enemyQueens);
+}
+
 U64 getAttacks(
     U64 defenderKing,
     U64 allDefenders,
--- MoveGen.h
+++ MoveGen.h
@@ -2,6 +2,16 @@
 #define RAY_MOVEGEN_H
 
 #include "Defs.h"
+#include "Move.h"
+
+static U64 safe;
+static U64 resolvers;
+static U64 cardinalPins;
+static U64 ordinalPins;
+
+static U64 getWhitePawnAttacks(U64 pawns);
+static U64 getBlackPawnAttacks(U64 pawns);
+
 
 static U64 getAttacks(
     U64 defenderKing,
@@ -13,8 +23,18 @@ static U64 getAttacks(
     U64 attackerQueens,
     U64 attackerKing);
 
-static U64 getCardinalSlidingMoves(int from, U64 blockers);
-static U64 getOrdinalSlidingMoves(int from, U64 blockers);
+static U64 getResolverSquares(
+    int checkedKing,
+    U64 pawnAttacks,
+    U64 attackerKnights,
+    U64 attackerBishops,
+    U64 attackerRooks,
+    U64 attackerQueens);
+
+static inline void updateLegalityInfo();
+
+static inline U64 getCardinalSlidingMoves(int from, U64 blockers);
+static inline U64 getOrdinalSlidingMoves(int from, U64 blockers);
 
 
 #endif
