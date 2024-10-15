a26fc4eb3b95fefd751fcce3fee1bd26cde3d54a
Joe Chrisman <jchrisman@mail.csuchico.edu>
2024-10-15 15:05:07 -0700

    make seed different when two programs start simultaneously

--- main.c
+++ main.c
@@ -5,12 +5,28 @@
 #include "Uci.h"
 #include "AttackTables.h"
 
+#ifdef _WIN32
+    #include <windows.h>
+    #define getpid() GetCurrentProcessId()
+#else
+#include <unistd.h>
+#endif
+
+int getSeed()
+{
+#ifdef _WIN32
+    return time(NULL) ^ GetCurrentProcessId();
+#else
+    return time(NULL) ^ getpid();
+#endif
+}
+
 int main()
 {
-    srand(time(NULL));
+    srand(getSeed());
     initAttackTables();
 
-    printf("Ray version 1.1.0\n");
+    printf("Ray version 1.1.1\n");
     fflush(stdout);
 
     char input[4];
